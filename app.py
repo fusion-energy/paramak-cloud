ce80beb7d2e422cfc3de1ce81aa67b76bd6e182d
Jonathan <mail@jshimwell.com>
2021-11-11 01:04:29 +0000

    progress on input boxes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_core_components as dcc
+import dash.dependencies as dd
+from dash.dependencies import Input, Output, State
+import paramak
+import dash_daq as daq
 
 # @server.route('/static/<path:path>')
 # def serve_static(path):
@@ -10,26 +13,137 @@ import dash_core_components as dcc
 
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app = dash.Dash(__name__)
+app = dash.Dash(
+    __name__,
+    prevent_initial_callbacks=True,
+)
+
+# paramak.BallReactor()
+# def generate_input_boxes():
+
+#                     # html.Div(children=[
+#     input_boxes = 
+    
+
+    # html.H4('Customer Name',style={'display':'inline-block','margin-right':20}),
+    # dcc.Input(id='customer-name',type='text',placeholder='',style={'display':'inline-block'}),
+            #  ('inboard_tf_leg_radial_thickness', float),
+            # 'center_column_shield_radial_thickness',
+            # 'divertor_radial_thickness',
+            # 'inner_plasma_gap_radial_thickness',
+            # 'plasma_radial_thickness',
+            # 'outer_plasma_gap_radial_thickness',
+            # 'firstwall_radial_thickness',
+            # 'blanket_radial_thickness',
+            # 'blanket_rear_wall_radial_thickness',
+            # # 'elongation',
+            # # 'triangularity',
+            # 'plasma_gap_vertical_thickness',
+            # 'divertor_to_tf_gap_vertical_thickness',
+            # 'number_of_tf_coils',
+            # 'rear_blanket_to_tf_gap',
+            # 'pf_coil_radial_thicknesses',
+            # 'pf_coil_vertical_thicknesses',
+            # 'pf_coil_radial_position',
+            # 'pf_coil_vertical_position',
+            # 'pf_coil_case_thicknesses',
+            # 'outboard_tf_coil_radial_thickness',
+            # 'outboard_tf_coil_poloidal_thickness',
+            # 'divertor_position',
+            # 'rotation_angle',
+        
+        # for input_arg in input_variable_names:
+        #     input_boxes.append(
+    # return input_boxes
+
 
 app.layout = html.Div(
     children=[
         html.H1(children='Paramak GUI'),
-        dcc.Dropdown(
-            id='reactor-selector',
-            options=[
-                {'label': 'BallReactor', 'value': 'NYC'},
-                {'label': 'S', 'value': 'MTL'},
-                {'label': 'San Francisco', 'value': 'SF'}
-            ],
-            value='NYC'
+        html.Div([
+                html.H4('inboard_tf_leg_radial_thickness', style={'display':'inline-block'}),
+                dcc.Input(id='inboard_tf_leg_radial_thickness', value=10,style={'display':'inline-block'}),
+                html.Br(),
+                html.H4('rotation_angle', style={'display':'inline-block'}),
+                dcc.Input(id='rotation_angle',value=16,style={'display':'inline-block'})
+            ]),
+        # html.Div(id="reactor_viewer"),
+        dcc.Loading(
+            id="loading-1",
+            type="default",
+            # children=html.Div(
+            #     id="reactor_viewer"
+            children=html.Iframe(id='new_reactor_viewer', target='_parent', height=600, width=1000, src="assets/reactor_3d.html")
+            # )
         ),
-        html.Iframe(height=600, width=1000, src="assets/reactor_3d.html")
+        # html.Iframe(id='new_reactor_viewer', height=600, width=1000, src="assets/reactor_3d.html"),
+        html.Button(
+            "Update reactor",
+            title="Click to update the reactor model with the latest parameters",
+            id="reactor_update",
+        )
     ]
 )
 
+@app.callback(
+    Output("new_reactor_viewer", "src"),
+    # Output("reactor_viewer", "children"),
+    Input("inboard_tf_leg_radial_thickness", "value"),
+    Input("rotation_angle", "value"),
+)
+def update_reactor(inboard_tf_leg_radial_thickness, rotation_angle):
+    my_reactor = paramak.BallReactor(
+        inboard_tf_leg_radial_thickness=float(inboard_tf_leg_radial_thickness),
+        rotation_angle=float(rotation_angle)
+    )
+    my_reactor.export_html_3d("assets/reactor_3d.html")
+    print('new assets/reactor_3d.html')
+    return 'new assets/reactor_3d.html'
+    # return html.Iframe(id='new_reactor_viewer', height=600, width=1000, src="assets/reactor_3d.html")
 
+
+# @app.callback(
+#     Output("reactor_viewer", "children"),
+#     Input("reactor_update", "n_clicks"),
+#     State("inboard_tf_leg_radial_thickness", "value"),
+#     State("rotation_angle", "value"),
+# )
+# def clicked_update_reactor(n_clicks, inboard_tf_leg_radial_thickness, rotation_angle):
+#     trigger_id = dash.callback_context.triggered[0]["prop_id"].split(".")[0]
+#     if trigger_id == "reactor_update":
+#         if n_clicks is None or n_clicks==0:
+#             raise dash.exceptions.PreventUpdate
+#         else:
+#             my_reactor = paramak.BallReactor(
+#                 inboard_tf_leg_radial_thickness=float(inboard_tf_leg_radial_thickness),
+#                 rotation_angle=float(rotation_angle)
+#             )
+#             my_reactor.export_html_3d("assets/reactor_3d.html")
+#             print('new assets/reactor_3d.html')
+#             return html.Iframe(id=f'{n_clicks}new_reactor_viewer', height=600, width=1000, src="assets/reactor_3d.html")
+
+# @app.callback(
+#     Output("reactor_input_args", "children"),
+#     Input("reactor-selector", "value")
+# )
+# def update_graph_from_mcnp(selected_reactor):
+#     if selected_reactor == 'BallReactor':
+#         my_reactor = paramak.BallReactor()
+#         input_boxes = []
+
+
+#         for input_arg in input_variable_names:
+#             input_boxes.append(
+#                 html.Div(children=[
+#                     html.P(input_arg[0]),
+#                     daq.NumericInput(id=input_arg[0])
+#                 ]
+#             )
+#         )
+#         return input_boxes
+#     else:
+#         return []
 
 
 if __name__ == '__main__':
-    app.run_server(port=8050, host='0.0.0.0')
+    app.run_server(port=8050, host='0.0.0.0', debug=True)
